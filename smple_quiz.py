3abd2d3d1281ba1fd1fae41d2b1e54e60297e734
Sunday Alabi <iamalabisunday@gmail.com>
2026-09-25 13:46:33 +0100

    initial commit

--- smple_quiz.py
+++ smple_quiz.py
@@ -1,3 +1,11 @@
+questions = [
+    ("What is the capital of Japan?", { "A" : "Beijing", "B" : "Seoul", "C" : "Tokyo", "D" : "Bangkok"}, "C")
+    ("Which country has the largest total land area?", { "A" : "Canada", "B" : "Russia", "C" : "China", "D" : "United States"}, "B")
+    ("Which country is known as the Land of the Rising Sun?", { "A" : "Japan", "B" : "Thailand", "C" : "South Korea", "D" : "Vietnam"}, "A")
+    ("What is the capital of Japan", { "A" : "Beijing", "B" : "Seoul", "C" : "Tokyo", "D" : "Bangkok"}, "C")
+    ("What is the capital of Japan", { "A" : "Beijing", "B" : "Seoul", "C" : "Tokyo", "D" : "Bangkok"}, "C")
+]
+
 while True:
 # -------------------------------------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------------------------------------
